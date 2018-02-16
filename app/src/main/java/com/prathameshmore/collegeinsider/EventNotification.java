1873d32b3a74fc85dee6d041791bd3ee79df6d88
Prathamesh More <moreprathameshmore@hotmail.com>
2018-02-16 21:45:11 +0530

    Added Event Notification

---
+++ app/src/main/java/com/prathameshmore/collegeinsider/EventNotification.java
@@ -0,0 +1,13 @@
+package com.prathameshmore.collegeinsider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventNotification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_notification);
+    }
+}
